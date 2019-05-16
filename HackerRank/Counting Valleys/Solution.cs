e80f85acb1761406591d7a9d4a6361975c994ce9
Vast <Vastlee@gmail.com>
2019-05-15 20:37:58 -0700

    Pulled & Solved Counting Valley. Pulled Forming a Magic Square

---
+++ HackerRank/Counting Valleys/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Counting_Valleys {
+  internal static class Solution {
+    static int countingValleys(int n, string s) {
+      int level = 0;
+      int stepDir;
+      int valleyCount = 0;
+
+      for(int i = 0; i < s.Length - 1; i++) {
+        stepDir = (s[i] == 'U') ? 1 : -1;
+        if((level >= 0) && (level + stepDir) < 0) {
+          valleyCount++;
+        }
+        level += stepDir;
+      }
+
+      return valleyCount;
+    }
+
+    static void Main(string[] args) {
+      TextWriter textWriter = new StreamWriter(@Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+      int n = Convert.ToInt32(Console.ReadLine());
+
+      string s = Console.ReadLine();
+
+      int result = countingValleys(n, s);
+
+      textWriter.WriteLine(result);
+
+      textWriter.Flush();
+      textWriter.Close();
+    }
+  }
+}
---
+++ HackerRank/Forming a Magic Square/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+class Solution {
+  #region Class Methods
+  private static int formingMagicSquare(int[][] s) {
+    int result = -1;
+
+    return result;
+  }
+
+  private static int FindCommonSum(int[][] s) {
+    int result = -1;
+    int[] sums = new int[3];
+    for(int i = 0; i < 3; i++) {
+
+    }
+    return result;
+  }
+
+  #endregion
+
+  #region Main
+  static void Main(string[] args) {
+    TextWriter textWriter = new StreamWriter(Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+    int[][] s = new int[3][];
+
+    for(int i = 0; i < 3; i++) {
+      s[i] = Array.ConvertAll(Console.ReadLine().Split(' '), sTemp => Convert.ToInt32(sTemp));
+    }
+
+    int result = formingMagicSquare(s);
+
+    textWriter.WriteLine(result);
+
+    textWriter.Flush();
+    textWriter.Close();
+  }
+  #endregion
+}
