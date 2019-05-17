9b6f6ac0401a406bec33c3153afb8619f83953d0
Vast <Vastlee@gmail.com>
2019-05-17 09:33:33 -0700

    Pulled & Solved Simple Array Sum

---
+++ HackerRank/Birthday Cake Candles/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Birthday_Cake_Candles {
+  internal static class Solution {
+    static int BirthdayCakeCandles(int[] ar) {
+      int heighest = -1;
+      int count = 0;
+
+      for(int i = 0; i < ar.Length; i++) {
+        if(ar[i] > heighest) {
+          heighest = ar[i];
+          count = 1;
+        } else if(ar[i] == heighest) { count++; }
+      }
+
+      return count;
+    }
+
+    static void Main(string[] args) {
+      TextWriter textWriter = new StreamWriter(Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+      int arCount = Convert.ToInt32(Console.ReadLine());
+      int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+      int result = BirthdayCakeCandles(ar);
+      textWriter.WriteLine(result);
+      textWriter.Flush();
+      textWriter.Close();
+    }
+  }
+}
---
+++ HackerRank/Simple Array Sum/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Simple_Array_Sum {
+  internal static class Solution {
+
+    private static int SimpleArraySum(int[] ar) {
+      int result = 0;
+      foreach(int e in ar) {
+        result += e;
+      }
+      return result;
+    }
+
+    private static void Main(string[] args) {
+      int arCount = Convert.ToInt32(Console.ReadLine());
+      int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+      int result = SimpleArraySum(ar);
+      Console.WriteLine(result);
+    }
+  }
+}
